8d8088cce27aae17aa2b50c3a969d89fed1549bd
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2014-08-06 22:41:34 +0200

    Migrate from distutils from setuptools
    Also revamp disthelpers.py.

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 #
 # clixtragen - generates helpers for a command line interpreter
 #
-# Copyright (C) 2013 Eric Le Bihan
+# Copyright (c) 2013 Eric Le Bihan <eric.le.bihan.dev@free.fr>
 #
-# This program is free software; you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -16,13 +16,12 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the
-# Free Software Foundation, Inc.,
-# 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from distutils.core import setup
-from disthelpers import build, build_trans, build_man, install_data
+from setuptools import setup, find_packages
+from disthelpers import extract_messages, init_catalog, update_catalog
+from disthelpers import build, build_catalog, build_man
 from clixtragen import __version__
 
 setup(name='clixtragen',
@@ -40,15 +39,19 @@ setup(name='clixtragen',
                    'Natural Language :: English',
                    'Development Status :: 2 - Pre-Alpha',
                    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',),
-      requires=['docutils (>=0.11)'],
-      packages=['clixtragen', 'clixtragen/generators', 'clixtragen/parsers'],
+      keywords=['command line', 'code generator'],
+      install_requires=['docutils >= 0.11'],
+      packages=find_packages(),
       scripts=['scripts/clixtragen'],
-      data_files=[('share/man/man1', ['build/man/man1/clixtragen.1'])],
+      data_files=[],
+      include_package_data=True,
       author='Eric Le Bihan',
       author_email='eric.le.bihan.dev@free.fr',
-      cmdclass = {'build': build,
-                  'build_man': build_man,
-                  'build_trans': build_trans,
-                  'install_data': install_data})
+      cmdclass={'build': build,
+                'build_man': build_man,
+                'extract_messages': extract_messages,
+                'init_catalog': init_catalog,
+                'update_catalog': update_catalog,
+                'build_catalog': build_catalog})
 
 # vim: ts=4 sts=4 sw=4 sta et ai
